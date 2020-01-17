59da86d851413f9c5fb39544be74fc262207f7cf
YAMAGUCHI Tatsuya <contact@ringsbell.net>
2020-01-17 13:51:06 +0900

    Initial Commit

---
+++ DocumentBasedAppWithOwnExtension/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DocumentBasedAppWithOwnExtension
+//
+//  Created by TATSUYA YAMAGUCHI on 2020/01/17.
+//  Copyright © 2020 TATSUYA YAMAGUCHI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
